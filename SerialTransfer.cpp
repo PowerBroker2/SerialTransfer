3a7770364a29a9d4dbe3f63a51e41a17ee6218ed
PB2 <ltbergeron@gmail.com>
2019-07-16 22:52:22 -0400

    Update SerialTransfer.cpp

--- SerialTransfer.cpp
+++ SerialTransfer.cpp
@@ -1,259 +1,222 @@
-/////////////////////////////////////////////////////////////////////////////////////////
+#include "SerialTransfer.h"
+
+
+
+
 /*
-   Command Array Anatomy:
+ bool SerialTransfer::begin(Stream *_port,
+                            uint8_t _dataSize,
+                            uint8_t _numFields,
+                            bool txOnly = false)
 
-   --------------------------------------------------------------------------------------
-   Index Number - Command Type                    - Command Format
-   --------------------------------------------------------------------------------------
-		0       - pitch                           - unsigned 16-Bit Analog
-		1       - roll                            - unsigned 16-Bit Analog
-		2       - yaw                             - unsigned 16-Bit Analog
-		3       - throttle                        - unsigned 16-Bit Analog
-		4       - Autopilot Toggle                - unsigned 16-Bit  Boolean (0x0 or 0x1)
-		5	- Pitch/Roll Limiter Toggle       - unsigned 16-Bit  Boolean (0x0 or 0x1)
-		6       - Landing Gear Toggle             - unsigned 16-Bit  Boolean (0x0 or 0x1)
-		7	- Flaps Toggle                    - unsigned 16-Bit  Boolean (0x0 or 0x1)
-		8       - Unused                          - Unused
-		9       - Unused                          - Unused
-		10      - Unused                          - Unused
-		11      - Unused                          - Unused
-		12      - Unused                          - Unused
-		13      - Unused                          - Unused
-		14      - Unused                          - Unused
-		15      - Unused                          - Unused
-		16      - Unused                          - Unused
-		17      - Unused                          - Unused
-		18      - Unused                          - Unused
-		19      - Unused                          - Unused
+ Description:
+ ------------
+  * Constructor for the SerialTransfer Class
+
+ Inputs:
+ -------
+  * Stream *_port - Pointer to Serial port to
+  communicate over
+  * uint8_t _dataSize - Number of bytes per field
+  * uint8_t _numFields - Number of fields per packet
+  * bool _txOnly = false - Whether or not data will be
+  received
+
+ Return:
+ -------
+  * bool - Whether or not the number of payload bytes
+  is within the required limits
 */
-/////////////////////////////////////////////////////////////////////////////////////////
+bool SerialTransfer::begin(Stream *_port,
+	                       uint8_t _dataSize,
+	                       uint8_t _numFields,
+	                       bool _txOnly = false)
+{
+	port = _port;
+	dataSize = _dataSize;
+	numFields = _numFields;
+	numPayBytes = numFields * dataSize;
+	txOnly = _txOnly;
 
-/////////////////////////////////////////////////////////////////////////////////////////
+	if (numPayBytes && (numPayBytes < 256))
+	{
+		// don't allocate array if only transmitting and not receiving
+		if (txOnly)
+		{
+			uint32_t len = (sizeof(uint8_t *) * numFields) + (sizeof(uint8_t) * dataSize * numFields);
+			inBuff = (uint8_t **)malloc(len);
+		}
+
+		return true;
+	}
+	
+	return false;
+}
+
+
+
+
 /*
-   Telemetry Array Anatomy:
+ bool SerialTransfer::sendData(uint8_t payload[])
 
-   --------------------------------------------------------------------------------------
-   Index Number - Telemetry Type                  - Telemetry Format
-   --------------------------------------------------------------------------------------
-		0       - Pitot Velocity (m/s) *100       - unsigned 16-bit  Analog
-		1       - Converted Altitude (m) * 100    - unsigned 16-bit  Analog
-		2       - Pitch Angle (degrees) * 100     - signed 16-bit  Analog
-		3       - Roll Angle (degrees) * 100      - signed 16-bit  Analog
-		4       - Latitude (dd) * 100             - signed 16-bit  Analog
-		5       - Longitude (dd) * 100            - signed 16-bit  Analog
-		6       - Unused                          - Unused
-		7       - Unused                          - Unused
-		8       - Unused                          - Unused
-		9       - Unused                          - Unused
-		10      - Unused                          - Unused
-		11      - Unused                          - Unused
-		12      - Unused                          - Unused
-		13      - Unused                          - Unused
-		14      - Unused                          - Unused
-		15      - Unused                          - Unused
-		16      - Unused                          - Unused
-		17      - Unused                          - Unused
-		18      - Unused                          - Unused
-		19      - Unused                          - Unused
+ Description:
+ ------------
+  * 
+
+ Inputs:
+ -------
+  * 
+
+ Return:
+ -------
+  * 
 */
-/////////////////////////////////////////////////////////////////////////////////////////
-
-#include "AirComms.h"
-
-#include "Shared_Tools.h"
-#include "IFC_Serial.h"
-#include "GS_Serial.h"
-
-
-
-
-//create GPS class
-airComms myRadio;
-
-
-
-
-void airComms::begin(bool _is_IFC)
+bool SerialTransfer::sendData(uint8_t payload[])
 {
-	//record if this is running on IFC or GS
-	is_IFC = _is_IFC;
+	if ((sizeof(payload) / sizeof(payload[0])) == numPayBytes)
+	{
+		uint8_t checksum = 0;
 
-	//set the propper serial ports
-	if (_is_IFC)
-	{
-		AIR_DEBUG_PORT = &IFC_DEBUG_PORT;
-		AIR_COMMAND_PORT = &IFC_COMMAND_PORT;
-		AIR_TELEM_PORT = &IFC_TELEM_PORT;
-	}
-	else
-	{
-		AIR_DEBUG_PORT = &GS_DEBUG_PORT;
-		AIR_COMMAND_PORT = &GS_COMMAND_PORT;
-		AIR_TELEM_PORT = &GS_TELEM_PORT;
+		checksum = findChecksum(payload);
+
+		port->write(START_BYTE);
+		port->write((uint8_t)(numPayBytes & 0xFF));
+		writePayload(payload);
+		port->write(checksum);
+		port->write(STOP_BYTE);
+
+		return true;
 	}
 
-	return;
+	return false;
 }
 
 
 
 
-//send data from outgoingArray
-void airComms::sendData()
+/*
+ void SerialTransfer::writePayload(byte payload[])
+
+ Description:
+ ------------
+  *
+
+ Inputs:
+ -------
+  *
+
+ Return:
+ -------
+  *
+*/
+void SerialTransfer::writePayload(byte payload[])
 {
-	byte k = 0;
-
-	//"build" the packet to transmit
-	outBuff[0] = START_BYTE;
-	outBuff[1] = 0;
-
-	//break all integers from outgoingArray apart into bytes and stuff them all into the packet
-	for (byte i = 0; i < (AIR_DATA_LEN * 2); i++)
-	{
-		if (i % 2)
-		{
-			//stuff LSB into the array
-			outBuff[i + 2] = outgoingArray[k] & 0xFF;
-
-			k++;
-		}
-		else
-		{
-			//reset the current value in the buffer
-			outBuff[i + 2] = 0;
-
-			//stuff MSB into the array
-			outBuff[i + 2] = (outgoingArray[k] >> 8) & 0xFF;
-		}
-	}
-
-	outBuff[BUFF_LEN - 1] = END_BYTE;
-	outBuff[1] = findChecksum(outBuff);
-
-	//send the packet one byte at a time
-	writePacket(outBuff);
-
-	return;
+	for (byte i = 0; i < (sizeof(payload) / sizeof(payload[0])); i++)
+		port->write(payload[i]);
 }
 
 
 
 
-//send the packet one byte at a time
-void airComms::writePacket(byte packet[])
-{
-	for (byte i = 0; i < BUFF_LEN; i++)
-	{
-		if (is_IFC)
-		{
-			AIR_TELEM_PORT->write(packet[i]);
-		}
-		else
-		{
-			AIR_COMMAND_PORT->write(packet[i]);
-		}
-	}
+/*
+ uint8_t SerialTransfer::findChecksum(uint8_t payload[])
 
-	return;
+ Description:
+ ------------
+  *
+
+ Inputs:
+ -------
+  *
+
+ Return:
+ -------
+  *
+*/
+uint8_t SerialTransfer::findChecksum(uint8_t payload[])
+{
+	uint8_t checksum = 0;
+
+	for (byte i = 0; i < (sizeof(payload) / sizeof(payload[0])); i++)
+		checksum += payload[i];
+
+	return ~checksum;
 }
 
 
 
 
-//update incomingArray with new data if available
-byte airComms::grabData_Radio()
+/*
+ int8_t SerialTransfer::available()
+
+ Description:
+ ------------
+  *
+
+ Inputs:
+ -------
+  *
+
+ Return:
+ -------
+  *
+*/
+int8_t SerialTransfer::available()
 {
-	int returnVal = AIR_NO_DATA;
-
-	//process the packet when a newline arrives:
-	if (arrayComplete_Radio)
+	if (!txOnly)
 	{
-		//process the data if the packet is uncorrupted
-		returnVal = extractData_Radio();
-
-		//reset the index counter
-		myRadio.inputArray_CurrentIndex = 0;
-
-		//reset the whole array
-		for (byte i = 0; i < BUFF_LEN; i++)
+		if (port->available())
 		{
-			myRadio.inputArray_Radio[i] = 0;
-		}
-
-		//reset complete flag
-		arrayComplete_Radio = false;
-	}
-
-	//nothing found
-	return returnVal;
-}
-
-
-
-
-//unpack and save received data packet
-int airComms::extractData_Radio()
-{
-	byte k = 0;
-	byte calculatedChecksum = 0;
-	byte receivedChecksum = 0;
-
-	//determine if a full packet was received
-	if ((inputArray_Radio[0] == START_BYTE) && (inputArray_Radio[BUFF_LEN-1] == END_BYTE))
-	{
-		//calculate checksum of the received packet
-		calculatedChecksum = findChecksum(inputArray_Radio);
-
-		//find received checksum from packet
-		receivedChecksum = inputArray_Radio[1];
-
-		//only process data if checksums match
-		if (calculatedChecksum == receivedChecksum)
-		{
-			//shuffle all bytes back into ints and save in incomingArray
-			for (byte i = 2; i < (((AIR_DATA_LEN * 2) - 1) + 2); i += 2)
+			while (port->available())
 			{
-				incomingArray[k] = (inputArray_Radio[i] << 8) | inputArray_Radio[i + 1];
+				char recChar = port->read();
 
-				k++;
+				if (startFound)
+				{
+					if (byteIndex == 0)
+					{
+						if (recChar != numPayBytes)
+							return PAYLOAD_ERROR;
+					}
+					else if (!payReceived)
+					{
+						inBuff[fieldIndex][subFieldIndex] = recChar;
+						
+						subFieldIndex++;
+						if (subFieldIndex == dataSize)
+						{
+							if ((fieldIndex == numFields) && (subFieldIndex == dataSize))
+								payReceived = true;
+
+							fieldIndex++;
+							subFieldIndex = 0;
+						}
+					}
+					else
+					{
+						if (byteIndex == (numPayBytes + 2))
+						{
+							// calculate and test checksum
+							// return if bad
+						}
+						else if (byteIndex == (numPayBytes + 3))
+						{
+							// test if END_BYTE was found
+							// reset everything
+							// return
+						}
+					}
+
+					byteIndex++;
+				}
+				else
+					if (recChar == START_BYTE)
+						startFound = true;
 			}
-
-			return AIR_NEW_DATA;
-		}
-		else
-		{
-			Serial.println("Checksum error");
-			Serial.println();
-			return AIR_CHECKSUM_ERROR;
 		}
 		
+		return NO_DATA;
 	}
-	else
-	{
-		Serial.println("Payload error");
-		return AIR_PAYLOAD_ERROR;
-	}
-}
-
-
-
-
-//find 8-bit checksum of message
-byte airComms::findChecksum(byte buff[])
-{
-	//initializw checksum
-	byte arrayChecksum = 0;
-
-	//compute checksum
-	for (byte i = 0; i < BUFF_LEN; i++)
-	{
-		//don't include the packet's checksum byte in checksum calculations
-		if (i != 1)
-			arrayChecksum = arrayChecksum + (byte)buff[i];
-	}
-
-	arrayChecksum = (~arrayChecksum) + 1;
-
-	//checksum updated
-	return arrayChecksum;
+	
+	return CONFIG_ERROR;
 }
