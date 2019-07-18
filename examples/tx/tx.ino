ba959411b6d1f006e9cd9d0a07748992cb7d6c14
PB2 <ltbergeron@gmail.com>
2019-07-17 21:56:16 -0400

    Update tx.ino

--- examples/tx/tx.ino
+++ examples/tx/tx.ino
@@ -1,9 +1,19 @@
+#include "SerialTransfer.h"
+
+#define NUM_FIELDS 3
+
+SerialTransfer myTransfer;
+
 void setup()
 {
-  
+  Serial.begin(115200);
+  Serial1.begin(115200);
+  myTransfer.begin(Serial1, NUM_FIELDS, 1, false);
 }
 
 void loop()
 {
-  
+  uint8_t data[NUM_FIELDS] = { 'h', 'i', '\n' };
+  Serial.println(myTransfer.sendData(data, (sizeof(data) / sizeof(data[0]))));
+  delay(1000);
 }
