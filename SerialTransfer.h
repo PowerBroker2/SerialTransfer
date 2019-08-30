08b761fefa7f4d2a3229a91d6464c9a28d96040d
PB2 <gitstuff2@gmail.com>
2019-08-30 14:48:32 -0400

    Added COBS encoding/decoding

--- SerialTransfer.h
+++ SerialTransfer.h
@@ -1,13 +1,14 @@
 /*
 
-01111110 00000000 00000000 00000000 ... 00000000 10000001
-|      | |      | |      | |      | | | |      | |______|__Stop byte
-|      | |      | |      | |      | | | |______|___________8-bit CRC
-|      | |      | |      | |      | |_|____________________Rest of payload
-|      | |      | |      | |______|________________________2nd payload byte
-|      | |      | |______|_________________________________1st payload byte
-|      | |______|__________________________________________# of payload bytes
-|______|___________________________________________________Start byte (constant)
+01111110 11111111 00000000 00000000 00000000 ... 00000000 10000001
+|      | |      | |      | |      | |      | | | |      | |______|__Stop byte
+|      | |      | |      | |      | |      | | | |______|___________8-bit CRC
+|      | |      | |      | |      | |      | |_|____________________Rest of payload
+|      | |      | |      | |      | |______|________________________2nd payload byte
+|      | |      | |      | |______|_________________________________1st payload byte
+|      | |      | |______|__________________________________________# of payload bytes
+|      | |______|___________________________________________________COBS Overhead byte
+|______|____________________________________________________________Start byte (constant)
 
 */
 
@@ -27,7 +28,7 @@ const int8_t STOP_BYTE_ERROR  = -3;
 const uint8_t START_BYTE      = 0x7E;
 const uint8_t STOP_BYTE       = 0x81;
 
-const uint8_t MAX_PACKET_SIZE = 0xFF;
+const uint8_t MAX_PACKET_SIZE = 0xFE;
 
 
 
@@ -53,6 +54,7 @@ public: // <<---------------------------------------//public
 private: // <<---------------------------------------//private
 	enum fsm {
 		find_start_byte,
+		find_overhead_byte,
 		find_payload_len,
 		find_payload,
 		find_checksum,
@@ -64,11 +66,17 @@ private: // <<---------------------------------------//private
 
 	uint8_t bytesToRec = 0;
 	uint8_t payIndex = 0;
+	uint8_t overheadByte = 0;
+	uint8_t recOverheadByte = 0;
 
 
 
 
 
 	uint8_t findChecksum(uint8_t arr[], uint8_t len);
+	void calcOverhead(uint8_t arr[], uint8_t len);
+	int16_t findLast(uint8_t arr[], uint8_t len);
+	void stuffPacket(uint8_t arr[], uint8_t len);
+	void unpackPacket(uint8_t arr[], uint8_t len);
 	void writePayload();
 };
