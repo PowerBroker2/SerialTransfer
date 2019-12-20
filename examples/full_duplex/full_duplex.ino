53d09f9ea2d5d7806273c6951a56a44bf7d41a56
PB2 <gitstuff2@gmail.com>
2019-12-19 22:30:34 -0500

    Create full_duplex.ino

---
+++ examples/full_duplex/full_duplex.ino
@@ -0,0 +1,36 @@
+#include "SerialTransfer.h"
+
+SerialTransfer myTransfer;
+
+void setup()
+{
+  Serial.begin(115200);
+  Serial1.begin(115200);
+  myTransfer.begin(Serial1);
+}
+
+void loop()
+{
+  float myFloat = 100.5;
+  
+  myTransfer.txBuff[0] = 'h';
+  myTransfer.txBuff[1] = 'i';
+  myTransfer.txBuff[2] = '\n';
+  
+  myTransfer.sendData(3);
+  delay(100);
+
+  while(!myTransfer.available())
+  {
+    if(myTransfer.status < 0)
+    {
+      Serial.print("ERROR: ");
+      Serial.println(myTransfer.status);
+    }
+  }
+
+  Serial.println("New Data");
+  for(byte i = 0; i < myTransfer.bytesRead; i++)
+    Serial.write(myTransfer.rxBuff[i]);
+  Serial.println();
+}
