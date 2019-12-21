465263098efdd5b2beb8891757f3aad50f9ea4ef
PB2 <gitstuff2@gmail.com>
2019-12-21 16:21:56 -0500

    Update tx.ino

--- examples/tx/tx.ino
+++ examples/tx/tx.ino
@@ -11,13 +11,10 @@ void setup()
 
 void loop()
 {
-  float myFloat = 100.5;
-  
   myTransfer.txBuff[0] = 'h';
   myTransfer.txBuff[1] = 'i';
   myTransfer.txBuff[2] = '\n';
-  myTransfer.txFloat(myFloat, 3); //insert the float "myFloat" at index 3 since "hi\n" already takes up indicies 0-2
   
-  myTransfer.sendData(7); //3 bytes for "hi\n" plus 4 bytes for the float "myFloat"
+  myTransfer.sendData(3);
   delay(100);
 }
