6e55586941b1359cbdc39d22e98ba1c356f161b4
PB2 <gitstuff2@gmail.com>
2019-12-21 16:20:48 -0500

    Update and rename examples/rx/rx.ino to examples/rx_simple/rx_simple.ino

--- examples/rx_simple/rx_simple.ino
+++ examples/rx_simple/rx_simple.ino
@@ -11,19 +11,11 @@ void setup()
 
 void loop()
 {
-  float myFloat;
-  
   if(myTransfer.available())
   {
-    /////////////////////////////////////////////////////////////// Handle Entire Packet
     Serial.println("New Data");
     for(byte i = 0; i < myTransfer.bytesRead; i++)
       Serial.write(myTransfer.rxBuff[i]);
-    Serial.println();
-
-    /////////////////////////////////////////////////////////////// Parse Out Float From Packet
-    myTransfer.rxFloat(myFloat, 3);
-    Serial.print("Received float: "); Serial.println(myFloat);
     Serial.println();
   }
   else if(myTransfer.status < 0)
