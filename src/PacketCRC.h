1fc62ab4c1196f58dd4473ac2458aa0e2eab628f
PB2 <gitstuff2@gmail.com>
2020-07-10 18:37:52 -0400

    Overhaul library architecture
    - Created a base library named `Packet.h` that serves as a hardware agnostic packetizing/parsing algorithm toolbox
    - `SerialTransfer.h` updated to reflect the lib architecture change

---
+++ src/PacketCRC.h
@@ -0,0 +1,79 @@
+#pragma once
+#include "Arduino.h"
+
+
+
+
+class PacketCRC
+{
+public: // <<---------------------------------------//public
+	uint8_t poly = 0;
+
+
+
+
+	PacketCRC(const uint8_t &polynomial=0x9B, const uint8_t &crcLen=8)
+	{
+		poly      = polynomial;
+		crcLen_   = crcLen;
+		tableLen_ = pow(2, crcLen);
+		csTable   = new uint8_t[tableLen_];
+
+		generateTable();
+	}
+
+	void generateTable()
+	{
+		for (int i = 0; i < tableLen_; ++i)
+		{
+			int curr = i;
+
+			for (int j = 0; j < 8; ++j)
+			{
+				if ((curr & 0x80) != 0)
+					curr = (curr << 1) ^ (int)poly;
+				else
+					curr <<= 1;
+			}
+
+			csTable[i] = (byte)curr;
+		}
+	}
+
+	void printTable()
+	{
+		for (int i = 0; i < tableLen_; i++)
+		{
+			Serial.print(csTable[i], HEX);
+
+			if ((i + 1) % 16)
+				Serial.print(' ');
+			else
+				Serial.println();
+		}
+	}
+
+	uint8_t calculate(const uint8_t &val)
+	{
+		if (val < tableLen_)
+			return csTable[val];
+		return 0;
+	}
+
+	uint8_t calculate(uint8_t arr[], uint8_t len)
+	{
+		uint8_t crc = 0;
+		for (uint16_t i = 0; i < len; i++)
+			crc = csTable[crc ^ arr[i]];
+
+		return crc;
+	}
+
+
+
+
+private: // <<---------------------------------------//private
+	uint16_t tableLen_;
+	uint8_t  crcLen_;
+	uint8_t* csTable;
+};
--- src/SerialTransfer.h
+++ src/SerialTransfer.h
@@ -1,50 +1,6 @@
-/*
-01111110 00000000 11111111 00000000 00000000 00000000 ... 00000000 10000001
-|      | |      | |      | |      | |      | |      | | | |      | |______|__Stop byte
-|      | |      | |      | |      | |      | |      | | | |______|___________8-bit CRC
-|      | |      | |      | |      | |      | |      | |_|____________________Rest of payload
-|      | |      | |      | |      | |      | |______|________________________2nd payload byte
-|      | |      | |      | |      | |______|_________________________________1st payload byte
-|      | |      | |      | |______|__________________________________________# of payload bytes
-|      | |      | |______|___________________________________________________COBS Overhead byte
-|      | |______|____________________________________________________________Packet ID (0 by default)
-|______|_____________________________________________________________________Start byte (constant)
-*/
-
 #pragma once
 #include "Arduino.h"
-#include "ST_CRC.h"
-
-
-
-
-typedef void(*functionPtr)();
-
-
-
-
-const int8_t CONTINUE         = 3;
-const int8_t NEW_DATA         = 2;
-const int8_t NO_DATA          = 1;
-const int8_t CRC_ERROR        = 0;
-const int8_t PAYLOAD_ERROR    = -1;
-const int8_t STOP_BYTE_ERROR  = -2;
-
-const uint8_t START_BYTE      = 0x7E;
-const uint8_t STOP_BYTE       = 0x81;
-
-const uint8_t MAX_PACKET_SIZE = 0xFE;
-
-
-
-
-struct configST {
-	Stream* port           = &Serial;
-	Stream* debugPort      = &Serial;
-	bool debug             = true;
-	functionPtr* callbacks = NULL;
-	uint8_t callbacksLen   = 0;
-};
+#include "Packet.h"
 
 
 
@@ -52,18 +8,13 @@ struct configST {
 class SerialTransfer
 {
 public: // <<---------------------------------------//public
-	SerialTransferCRC crc;
-
-	uint8_t txBuff[MAX_PACKET_SIZE];
-	uint8_t rxBuff[MAX_PACKET_SIZE];
-
 	uint8_t bytesRead = 0;
-	int8_t status = 0;
+	int8_t status     = 0;
 
 
 
 
-	void begin(const configST configs);
+	void begin(Stream &_port, const configST configs);
 	void begin(Stream &_port, const bool _debug=true, Stream &_debugPort=Serial);
 	uint8_t sendData(const uint16_t &messageLen, const uint8_t packetID=0);
 	uint8_t available();
@@ -80,7 +31,6 @@ public: // <<---------------------------------------//public
 	  * Stuffs "len" number of bytes of an arbitrary object (byte, int,
 	  float, double, struct, etc...) into the transmit buffer (txBuff)
 	  starting at the index as specified by the argument "index"
-
 	 Inputs:
 	 -------
 	  * const T &val - Pointer to the object to be copied to the
@@ -88,7 +38,6 @@ public: // <<---------------------------------------//public
 	  * const uint16_t &index - Starting index of the object within the
 	  transmit buffer (txBuff)
 	  * const uint16_t &len - Number of bytes of the object "val" to transmit
-
 	 Return:
 	 -------
 	  * uint16_t maxIndex - uint16_t maxIndex - Index of the transmit buffer (txBuff) that directly follows the bytes processed
@@ -97,21 +46,7 @@ public: // <<---------------------------------------//public
 	template <typename T>
 	uint16_t txObj(const T &val, const uint16_t &index=0, const uint16_t &len=sizeof(T))
 	{
-		uint8_t* ptr = (uint8_t*)&val;
-		uint16_t maxIndex;
-
-		if ((len + index) > MAX_PACKET_SIZE)
-			maxIndex = MAX_PACKET_SIZE;
-		else
-			maxIndex = len + index;
-
-		for (uint16_t i = index; i < maxIndex; i++)
-		{
-			txBuff[i] = *ptr;
-			ptr++;
-		}
-
-		return maxIndex;
+		return packet.txObj(val, index, len);
 	}
 
 
@@ -124,7 +59,6 @@ public: // <<---------------------------------------//public
 	  * Reads "len" number of bytes from the receive buffer (rxBuff)
 	  starting at the index as specified by the argument "index"
 	  into an arbitrary object (byte, int, float, double, struct, etc...)
-
 	 Inputs:
 	 -------
 	  * const T &val - Pointer to the object to be copied into from the
@@ -132,7 +66,6 @@ public: // <<---------------------------------------//public
 	  * const uint16_t &index - Starting index of the object within the
 	  receive buffer (rxBuff)
 	  * const uint16_t &len - Number of bytes in the object "val" received
-
 	 Return:
 	 -------
 	  * uint16_t maxIndex - Index of the receive buffer (rxBuff) that directly follows the bytes processed
@@ -141,57 +74,40 @@ public: // <<---------------------------------------//public
 	template <typename T>
 	uint16_t rxObj(const T &val, const uint16_t &index=0, const uint16_t &len=sizeof(T))
 	{
-		uint8_t* ptr = (uint8_t*)&val;
-		uint16_t maxIndex;
-
-		if ((len + index) > MAX_PACKET_SIZE)
-			maxIndex = MAX_PACKET_SIZE;
-		else
-			maxIndex = len + index;
-
-		for (uint16_t i = index; i < maxIndex; i++)
-		{
-			*ptr = rxBuff[i];
-			ptr++;
-		}
-
-		return maxIndex;
+		return packet.rxObj(val, index, len);
 	}
 
 
 
 
+	/*
+	 uint8_t SerialTransfer::sendDatum(const T &val, const uint16_t &len=sizeof(T))
+	 Description:
+	 ------------
+	  * Stuffs "len" number of bytes of an arbitrary object (byte, int,
+	  float, double, struct, etc...) into the transmit buffer (txBuff)
+	  starting at the index as specified by the argument "index" and
+	  automatically transmits the bytes in an individual packet
+	 Inputs:
+	 -------
+	  * const T &val - Pointer to the object to be copied to the
+	  transmit buffer (txBuff)
+	  * const uint16_t &len - Number of bytes of the object "val" to transmit
+	 Return:
+	 -------
+	  * uint8_t - Number of payload bytes included in packet
+	*/
+	template <typename T>
+	uint8_t sendDatum(const T &val, const uint16_t &len=sizeof(T))
+	{
+		return sendData(packet.txObj(val, 0, len));
+	}
+
+
+
+
+
 private: // <<---------------------------------------//private
-	enum fsm {
-		find_start_byte,
-		find_id_byte,
-		find_overhead_byte,
-		find_payload_len,
-		find_payload,
-		find_crc,
-		find_end_byte
-	};
-	fsm state = find_start_byte;
-
-	functionPtr* callbacks = NULL;
-	uint8_t callbacksLen = 0;
-
+	Packet packet;
 	Stream* port;
-	Stream* debugPort;
-	bool debug = false;
-
-	uint8_t bytesToRec = 0;
-	uint8_t payIndex = 0;
-	uint8_t idByte = 0;
-	uint8_t overheadByte = 0;
-	uint8_t recOverheadByte = 0;
-
-
-
-
-
-	void calcOverhead(uint8_t arr[], const uint8_t &len);
-	int16_t findLast(uint8_t arr[], const uint8_t &len);
-	void stuffPacket(uint8_t arr[], const uint8_t &len);
-	void unpackPacket(uint8_t arr[], const uint8_t &len);
 };
