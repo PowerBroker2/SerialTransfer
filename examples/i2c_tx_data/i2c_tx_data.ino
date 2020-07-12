d6dbb0d3ff155424ed82cd76e8fe0be8a95813de
PB2 <gitstuff2@gmail.com>
2020-07-11 21:31:04 -0400

    Add I2C examples

---
+++ examples/i2c_tx_data/i2c_tx_data.ino
@@ -0,0 +1,43 @@
+#include "I2CTransfer.h"
+
+
+I2CTransfer myTransfer;
+
+struct STRUCT {
+  char z;
+  float y;
+} testStruct;
+
+char arr[] = "hello";
+
+
+void setup()
+{
+  Serial.begin(115200);
+  Wire.begin();
+
+  configST myConfig;
+  myConfig.debug = true;
+  myTransfer.begin(Wire, myConfig);
+
+  testStruct.z = '~';
+  testStruct.y = 4.5;
+}
+
+
+void loop()
+{
+  // use this variable to keep track of how many
+  // bytes we're stuffing in the transmit buffer
+  uint16_t sendSize = 0;
+
+  ///////////////////////////////////////// Stuff buffer with struct
+  sendSize = myTransfer.txObj(testStruct, sendSize);
+
+  ///////////////////////////////////////// Stuff buffer with array
+  sendSize = myTransfer.txObj(arr, sendSize);
+
+  ///////////////////////////////////////// Send buffer
+  myTransfer.sendData(sendSize);
+  delay(500);
+}
