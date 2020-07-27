10411454875bda37f343f223a54484b56aba778f
Yannick Schinko <y.schinko@connectingmedia.de>
2020-07-27 17:03:38 +0200

    Updated I2C examples

--- examples/i2c_rx_data/i2c_rx_data.ino
+++ examples/i2c_rx_data/i2c_rx_data.ino
@@ -3,51 +3,40 @@
 
 I2CTransfer myTransfer;
 
-struct STRUCT {
-  char z;
-  float y;
+struct STRUCT
+{
+	char  z;
+	float y;
 } testStruct;
 
 char arr[6];
 
 
 /////////////////////////////////////////////////////////////////// Callbacks
-void hi()
+void handlePacket(Packet& packet)
 {
-  // use this variable to keep track of how many
-  // bytes we've processed from the receive buffer
-  uint16_t recSize = 0;
+	packet.rxObj(testStruct);
+	Serial.print(testStruct.z);
+	Serial.print(testStruct.y);
+	Serial.print(" | ");
 
-  recSize = myTransfer.rxObj(testStruct, recSize);
-  Serial.print(testStruct.z);
-  Serial.print(testStruct.y);
-  Serial.print(" | ");
-
-  recSize = myTransfer.rxObj(arr, recSize);
-  Serial.println(arr);
+	packet.rxObj(arr);
+	Serial.println(arr);
 }
 ///////////////////////////////////////////////////////////////////
 
 
 void setup()
 {
-  Serial.begin(115200);
-  Wire.begin(0);
+	Serial.begin(115200);
+	Wire.begin(0);
 
-  functionPtr callbackArr[] = { hi };
-
-  ///////////////////////////////////////////////////////////////// Config Parameters
-  configST myConfig;
-  myConfig.debug        = true;
-  myConfig.callbacks    = callbackArr;
-  myConfig.callbacksLen = sizeof(callbackArr) / sizeof(functionPtr);
-  /////////////////////////////////////////////////////////////////
-  
-  myTransfer.begin(Wire, myConfig);
+	myTransfer.begin(Wire, Serial);
+	myTransfer.addCallback(handlePacket);
 }
 
 
 void loop()
 {
-  // Do nothing
+	myTransfer.tick();
 }
