e420907c3a5ed876b158f94c2cb2a8f92f5d95c1
PB2 <gitstuff2@gmail.com>
2020-10-18 18:33:28 -0400

    Define `DISABLE_SPI_SERIALTRANSFER`

--- src/SerialTransfer.h
+++ src/SerialTransfer.h
@@ -3,6 +3,9 @@
 #include "Packet.h"
 
 
+#define DISABLE_SPI_SERIALTRANSFER 1 // Comment out to use SPITransfer.h
+
+
 class SerialTransfer
 {
   public: // <<---------------------------------------//public
